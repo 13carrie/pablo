7a5ab3cc0bae60d3216ae7b883f3b36227ec339c
Caroline Smith <caroline.2.smith@kcl.ac.uk>
2024-03-15 10:50:39 +0800

    Implement data preprocessing function so CICIDS doesn't have to be preprocessed multiple times

--- Scripts/preprocess_cic.py
+++ Scripts/preprocess_cic.py
@@ -1,3 +1,59 @@
-# ensure that csvs don't contain two fwd header length columns
+import os
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import IntegerType
+import sys
 
-# do all data preprocessing from pablo_rf etc
+# do all data preprocessing for a csv file and store it in the data/processed directory for easy use by models
+
+print("Pyspark version: {}".format(pyspark.__version__))
+
+spark = SparkSession.builder.appName("Preprocess CICIDS2017 and Improved CICIDS2017 Data") \
+    .config("spark.driver.memory", "15g") \
+    .config("spark.master", "local") \
+    .getOrCreate()
+
+
+# preprocess a directory of csv files, assuming that said directory is stored in code/data/raw directory
+# note that writing your dataframe to disk may automatically partition the dataframe into multiple partial files within
+# the new csv_dir directory
+def preprocess_data():
+    csv_dir = sys.argv[1]
+    rel_csv_path = "/Data/Raw/" + csv_dir
+    df = load_data(rel_csv_path)
+    df = clean_dataframe(df)
+    df = create_ground_truth(df)
+    df.printSchema()
+    new_csv_path = os.getcwd() + "/Data/Processed/" + csv_dir
+    df.write.option("header", True).mode("overwrite").csv(new_csv_path)
+
+
+
+# Takes a path to a directory as a param and returns a Spark DataFrame with supplied headers
+def load_data(csv_path: str) -> DataFrame:
+    # Loading CSV files, and merging all of them into a single DataFrame
+    cwd = os.getcwd()
+    abs_path = cwd + "/" + csv_path
+    df = spark.read.csv(abs_path, header=True, inferSchema=True)
+
+    print("New dataframe created")
+    return df
+
+
+# Deletes rows with NaN/infinite values for a feature
+def clean_dataframe(df: DataFrame) -> DataFrame:
+    df = df.replace(to_replace=[np.inf, -np.inf], value=None)
+    df = df.dropna(how="any")  # Deleting any rows with null/None values
+    df = df.drop_duplicates()  # Deleting rows with duplicate values for all features
+    return df
+
+
+# Uses binary encoding to convert labels to 1 (malicious) or 0 (benign) values
+def create_ground_truth(df: DataFrame) -> DataFrame:
+    df = df.withColumn(" GT", when(df[" Label"] == 'BENIGN', '1').otherwise('0'))
+    df = df.withColumn(" GT", col(" GT").cast(IntegerType()))
+    return df
+
+
+preprocess_data()
--- pablo_rf_model_1.py
+++ pablo_rf_model_1.py
@@ -11,8 +11,9 @@ import pyspark
 from pyspark.ml import Pipeline
 from pyspark.ml.classification import RandomForestClassifier
 from pyspark.sql import SparkSession
-from pyspark.ml.feature import StringIndexer, VectorAssembler
-from pyspark.sql.functions import *
+from pyspark.ml.feature import VectorAssembler
+# from pyspark.sql.functions import *
+from datetime import datetime
 
 print("Pyspark version: {}".format(pyspark.__version__))
 
@@ -23,49 +24,38 @@ spark = SparkSession.builder.appName("Pablo Experiment 1")\
 
 print("Created new Spark Session")
 
-# Reading CSV files, and merging all of them into a single DataFrame
-original_csvs = "/Data/Raw/MachineLearningCVE/"
+# Loading preprocessed CSV files, and merging all of them into a single DataFrame
+original_csvs = "/Data/Processed/MachineLearningCVE/"
 cwd = os.getcwd()
 abs_path = cwd + original_csvs
 df = spark.read.csv(abs_path, header=True, inferSchema=True)
 
 print("New dataframe created")
 
-# PREPROCESSING:
-# Deleting rows with NaN/infinite values for a feature
-df = df.replace(to_replace=[np.inf, -np.inf], value=None)
-df = df.dropna(how="any")  # Deleting any rows with null/None values
-df = df.drop_duplicates()  # Deleting rows with duplicate values for all features
-
-# Create a new column GT that encodes all malicious classes into a single class for binary classification
-# set benign samples = 1, malicious samples = 0
-df = df.withColumn(" Label", when(df[" Label"] == 'BENIGN', 'Benign').otherwise('Malicious'))
-stage_1 = StringIndexer(inputCol=" Label", outputCol=" GT")  # create ground truth column
-
 # Define the features used by the classifier
-features = [' Destination Port', ' Flow Duration', ' Total Fwd Packets', ' Total Backward Packets',
-            'Total Length of Fwd Packets', ' Total Length of Bwd Packets', ' Fwd Packet Length Max',
-            ' Fwd Packet Length Min', ' Fwd Packet Length Mean', ' Fwd Packet Length Std', 'Bwd Packet Length Max',
-            ' Bwd Packet Length Min', ' Bwd Packet Length Mean', ' Bwd Packet Length Std', 'Flow Bytes/s',
-            ' Flow Packets/s', ' Flow IAT Mean', ' Flow IAT Std', ' Flow IAT Max', ' Flow IAT Min', 'Fwd IAT Total',
-            ' Fwd IAT Mean', ' Fwd IAT Std', ' Fwd IAT Max', ' Fwd IAT Min', 'Bwd IAT Total', ' Bwd IAT Mean',
-            ' Bwd IAT Std', ' Bwd IAT Max', ' Bwd IAT Min', 'Fwd PSH Flags', ' Bwd PSH Flags', ' Fwd URG Flags',
-            ' Bwd URG Flags', ' Fwd Header Length', ' Bwd Header Length', 'Fwd Packets/s', ' Bwd Packets/s',
-            ' Min Packet Length', ' Max Packet Length', ' Packet Length Mean', ' Packet Length Std',
-            ' Packet Length Variance', 'FIN Flag Count', ' SYN Flag Count', ' RST Flag Count', ' PSH Flag Count',
-            ' ACK Flag Count', ' URG Flag Count', ' CWE Flag Count', ' ECE Flag Count', ' Down/Up Ratio',
-            ' Average Packet Size', ' Avg Fwd Segment Size', ' Avg Bwd Segment Size', 'Fwd Avg Bytes/Bulk',
-            ' Fwd Avg Packets/Bulk', ' Fwd Avg Bulk Rate', ' Bwd Avg Bytes/Bulk', ' Bwd Avg Packets/Bulk',
-            'Bwd Avg Bulk Rate', 'Subflow Fwd Packets', ' Subflow Fwd Bytes', ' Subflow Bwd Packets',
-            ' Subflow Bwd Bytes', 'Init_Win_bytes_forward', ' Init_Win_bytes_backward', ' act_data_pkt_fwd',
-            ' min_seg_size_forward', 'Active Mean', ' Active Std', ' Active Max', ' Active Min', 'Idle Mean',
-            ' Idle Std', ' Idle Max', ' Idle Min']
+features = ['Destination Port', 'Flow Duration', 'Total Fwd Packets', 'Total Backward Packets',
+            'Total Length of Fwd Packets', 'Total Length of Bwd Packets', 'Fwd Packet Length Max',
+            'Fwd Packet Length Min', 'Fwd Packet Length Mean', 'Fwd Packet Length Std', 'Bwd Packet Length Max',
+            'Bwd Packet Length Min', 'Bwd Packet Length Mean', 'Bwd Packet Length Std', 'Flow Bytes/s',
+            'Flow Packets/s', 'Flow IAT Mean', 'Flow IAT Std', 'Flow IAT Max', 'Flow IAT Min', 'Fwd IAT Total',
+            'Fwd IAT Mean', 'Fwd IAT Std', 'Fwd IAT Max', 'Fwd IAT Min', 'Bwd IAT Total', 'Bwd IAT Mean',
+            'Bwd IAT Std', 'Bwd IAT Max', 'Bwd IAT Min', 'Fwd PSH Flags', 'Bwd PSH Flags', 'Fwd URG Flags',
+            'Bwd URG Flags', 'Fwd Header Length', 'Bwd Header Length', 'Fwd Packets/s', 'Bwd Packets/s',
+            'Min Packet Length', 'Max Packet Length', 'Packet Length Mean', 'Packet Length Std',
+            'Packet Length Variance', 'FIN Flag Count', 'SYN Flag Count', 'RST Flag Count', 'PSH Flag Count',
+            'ACK Flag Count', 'URG Flag Count', 'CWE Flag Count', 'ECE Flag Count', 'Down/Up Ratio',
+            'Average Packet Size', 'Avg Fwd Segment Size', 'Avg Bwd Segment Size', 'Fwd Avg Bytes/Bulk',
+            'Fwd Avg Packets/Bulk', 'Fwd Avg Bulk Rate', 'Bwd Avg Bytes/Bulk', 'Bwd Avg Packets/Bulk',
+            'Bwd Avg Bulk Rate', 'Subflow Fwd Packets', 'Subflow Fwd Bytes', 'Subflow Bwd Packets',
+            'Subflow Bwd Bytes', 'Init_Win_bytes_forward', 'Init_Win_bytes_backward', 'act_data_pkt_fwd',
+            'min_seg_size_forward', 'Active Mean', 'Active Std', 'Active Max', 'Active Min', 'Idle Mean',
+            'Idle Std', 'Idle Max', 'Idle Min']
 stage_2 = VectorAssembler(inputCols=features, outputCol="features")
 
 # create random forest classifier with hyperparams of gints + engelen's work
-stage_3 = RandomForestClassifier(maxDepth=30, numTrees=100, labelCol=" GT", featuresCol="features")
+stage_3 = RandomForestClassifier(maxDepth=30, numTrees=100, labelCol="GT", featuresCol="features")
 
-pipeline = Pipeline(stages=[stage_1, stage_2, stage_3])
+pipeline = Pipeline(stages=[stage_2, stage_3])
 
 training_data, test_data = df.randomSplit([0.7, 0.3])
 print("Train/Test data split successful.")
@@ -79,5 +69,7 @@ end = time.time() - start
 print("Training pipeline time: ", end)
 
 print("Saving model...")
-model_path = cwd + "/Scripts/Trained_Models/" + "/pablo_model_1"
+now = datetime.now()
+
+model_path = cwd + "/Scripts/Trained_Models/pablo_model_1" + now.strftime("%d/%m-%H:%M")
 rf_model.save(model_path)
