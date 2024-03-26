c8ca49c6249c73c8b76a97b0c54e9452edd1dd56
Caroline Smith <caroline.2.smith@kcl.ac.uk>
2024-03-26 09:49:01 +0000

    Implement multi-core SHAP value calculation

--- Scripts/evaluate_model.py
+++ Scripts/evaluate_model.py
@@ -1,5 +1,9 @@
 import os
 import sys
+from collections.abc import Iterator
+
+import numpy as np
+import pandas as pd
 import shap
 import time
 # import numpy as np
@@ -10,6 +14,7 @@ from pyspark.ml.feature import VectorAssembler
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.sql.types import StructType, StructField, FloatType
 
 spark = SparkSession.builder.appName("Pablo Evaluation") \
     .config("spark.driver.memory", "15g") \
@@ -25,7 +30,8 @@ def evaluate_model():
     saved_model = RandomForestClassificationModel.load(abs_model_path)
 
     abs_test_dir_path = get_test_data_directory(model_id)  # try to load test data with filename as [test_data-model_id]
-    test_df = spark.read.csv(abs_test_dir_path, header=True, inferSchema=True) # recreating test dataframe from test csv
+    test_df = spark.read.csv(abs_test_dir_path, header=True,
+                             inferSchema=True)  # recreating test dataframe from test csv
     features_to_remove = ["GT"]  # features to not be included in training for model
     col_list = test_df.columns
     features = list(set(col_list) - set(features_to_remove))
@@ -43,7 +49,7 @@ def evaluate_model():
     print("Time to get prediction outputs: ", pred_end)
     # print(predictions_df.head())
 
-    # # get performance metrics of saved model when tested on test_df, using predictions_df
+    # get performance metrics of saved model when tested on test_df, using predictions_df
     # metric_start = time.time()
     # get_prediction_metrics(predictions_df)
     # metric_end = time.time() - metric_start
@@ -52,7 +58,9 @@ def evaluate_model():
     # get shap values
     test_df.drop("features")
     print("Beginning SHAP value computation...")
-    shap_values = get_shap_values(saved_model, test_df.toPandas())
+    test_df = test_df.toPandas()
+    shap_values = get_shap_values_multicore(saved_model, test_df)
+
 
 # returns 'unique' model information
 def get_unique_model_info():
@@ -61,23 +69,23 @@ def get_unique_model_info():
     model_id = model_name[-15:]  # model_id = isolate unique model id from trained_model file name
     return model_name, model_path, model_id
 
+
 # returns absolute path to test data directory
 def get_test_data_directory(model_id):
     test_dir = "/Data/Processed-Test/" + "test_data-" + model_id  # directory of test data file that matches with model
     abs_test_dir_path = cwd + test_dir  # absolute path of test data directory
     return abs_test_dir_path
 
+
 # prints accuracy, precision, recall, f1, auc scores for model predictions on test data
 def get_prediction_metrics(predictions_df: DataFrame):
     # assumption that predictions dataframe contains 'features', 'rawPrediction', 'probability', 'prediction' columns
-    eval_accuracy = MulticlassClassificationEvaluator(labelCol="GT", predictionCol="prediction",
-                                                      metricName="accuracy")
+    eval_accuracy = MulticlassClassificationEvaluator(labelCol="GT", predictionCol="prediction", metricName="accuracy")
     eval_precision = MulticlassClassificationEvaluator(labelCol="GT", predictionCol="prediction",
                                                        metricName="precisionByLabel")
     eval_recall = MulticlassClassificationEvaluator(labelCol="GT", predictionCol="prediction",
                                                     metricName="recallByLabel")
     eval_f1 = MulticlassClassificationEvaluator(labelCol="GT", predictionCol="prediction", metricName="f1")
-
     eval_auc = BinaryClassificationEvaluator(labelCol="GT", rawPredictionCol="prediction")
 
     accuracy = eval_accuracy.evaluate(predictions_df)
@@ -94,24 +102,45 @@ def get_prediction_metrics(predictions_df: DataFrame):
     # print("Matthews Correlation Coefficient (MCC): {:3f}")
 
 
-# a multi-node implementation for getting shap values
-# def multi_get_shap_values(model: PipelineModel, df: DataFrame):
-#     return
-
-# a single-node implementation for getting shap values from a pandas df
+# the original single-node implementation for getting shap values from a pandas df
 def get_shap_values(model, test_df):
     # create shap explainer with values
-    test_df = test_df.drop(columns=['features']) # features' 'sparsevector' type is not compatible with shap
+    test_df = test_df.drop(columns=['features'])  # features' 'sparsevector' type is not compatible with shap
     explainer = shap.TreeExplainer(model)
     shap_values = explainer.shap_values(test_df, check_additivity=False)
     return shap_values
 
 
+# the multi-node implementation for getting shap values from a pandas df
+# calculate_shap udf from https://www.databricks.com/blog/2022/02/02/scaling-shap-calculations-with-pyspark-and-pandas-udf.html
+def get_shap_values_multicore(model, test_df):
+    start = time.time()
+    test_df = test_df.drop(columns=['features'])  # features column 'sparsevector' type is not compatible with shap
+    cols = test_df.columns
+    print("Creating SHAP explainer...")
+    explainer = shap.TreeExplainer(model)
+    print("Successfully created SHAP explainer")
+
+    def calculate_shap(iterator: Iterator[pd.DataFrame]) -> Iterator[pd.DataFrame]:
+        for X in iterator:
+            yield pd.DataFrame(
+                explainer.shap_values(np.array(X), check_additivity=False)[0],
+                columns=cols,
+            )
+
+    return_schema = StructType()
+    for feature in cols:
+        return_schema = return_schema.add(StructField(feature, FloatType()))
+    print("Built return schema")
+
+    spark_test_df = spark.createDataFrame(test_df)
+    print("converted df to spark")
+    shap_values = spark_test_df.mapInPandas(calculate_shap, schema=return_schema)
+    print("Got SHAP values woohoo")
+    end = time.time() - start
+    print("SHAP computation time: ", end)
+    return shap_values
+
+
 evaluate_model()
 
-
-
-# - **Use grid-search for automatic parameter tuning, or cross-validation (or repeated random samplings) to increase
-# the confidence of the results.** The notebook only trains (and tests) an ML saved_model once. The resulting
-# performance can be biased (e.g., it can be due to a lucky sampling for train or test). To derive more statistically
-# significant results, more trials should be done.
