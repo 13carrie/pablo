a5dc6346bd4e8c35323c1c748b93d401776b4661
Caroline Smith <caroline.2.smith@kcl.ac.uk>
2024-04-21 18:10:31 -0400

    add func to split_data and reorganise metrics

--- Scripts/get_metrics.py
+++ Scripts/get_metrics.py
@@ -5,9 +5,6 @@ import numpy as np
 import pandas as pd
 import shap
 import time
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import seaborn as sns
 from pyspark.sql.functions import when, col
 from pyspark.ml.classification import RandomForestClassificationModel
 from pyspark.ml.feature import VectorAssembler
@@ -114,5 +111,81 @@ def convert_to_binary_classification(predictions_df: DataFrame):
     predictions_df = predictions_df.withColumn("prediction", when(predictions_df["prediction"] == 0.0, 0.0).otherwise(1.0))
     return predictions_df
 
+def get_shap():
+    model_arg = sys.argv[1]  # retrieve model name stored under trained_models
+    model_name, abs_model_path, model_id = get_unique_model_info(model_arg)
+    print("Loading saved model...")
+    saved_model = RandomForestClassificationModel.load(abs_model_path)
 
-evaluate_model()
+    abs_preds_dir_path = get_predictions_data_directory(
+        model_id)  # try to load predictions data
+    pred_df = spark.read.csv(abs_preds_dir_path, header=True,
+                             inferSchema=True)
+
+    print("Beginning SHAP value computation...")
+    pred_df = pred_df.toPandas()
+    shap_values = get_shap_values_multicore(saved_model, pred_df)
+
+    plot_shap_force(shap_values)
+
+    other_shap_values, portscan_shap_values = isolate_gt_label(shap_values, 10.0)
+    other_shap_values, ssh_patator_shap_values = isolate_gt_label(other_shap_values, 11.0)
+    other_shap_values, ftp_patator_shap_values = isolate_gt_label(other_shap_values, 7.0)
+    patator_shap_values = ssh_patator_shap_values.union(ftp_patator_shap_values)
+    portscan_shap_values.write.option("header", True).mode("overwrite").csv(
+        (cwd + "/Data/Results/portscan_shap" + model_id))
+    patator_shap_values.write.option("header", True).mode("overwrite").csv(
+        (cwd + "/Data/Results/patator_shap" + model_id))
+
+
+# returns absolute path to test data directory
+def get_predictions_data_directory(model_id):
+    test_dir = "/Data/Results/predictions_data" + model_id  # directory of test data file that matches with model
+    abs_preds_dir_path = cwd + test_dir  # absolute path of test data directory
+    return abs_preds_dir_path
+
+
+# the original single-node implementation for getting shap values from a pandas df, not used due to lack of efficiency
+def get_shap_values(model, test_df):
+    # create shap explainer with values
+    test_df = test_df.drop(columns=['features'])  # features' 'sparsevector' type is not compatible with shap
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(test_df, check_additivity=False)
+    return shap_values
+
+
+# the multi-node implementation for getting shap values from a pandas df
+# calculate_shap udf and four lines below are from https://www.databricks.com/blog/2022/02/02/scaling-shap-calculations-with-pyspark-and-pandas-udf.html
+def get_shap_values_multicore(model, test_df):
+    start = time.time()
+    test_df = test_df
+    cols = test_df.columns
+    print("Creating SHAP explainer...")
+    explainer = shap.TreeExplainer(model)
+    print("Successfully created SHAP explainer")
+
+    # udf which calculates shap values in parallel
+    def calculate_shap(iterator: Iterator[pd.DataFrame]) -> Iterator[pd.DataFrame]:
+        for X in iterator:
+            yield pd.DataFrame(
+                explainer.shap_values(np.array(X), check_additivity=False)[0],
+                columns=cols,
+            )
+
+    return_schema = StructType()
+    for feature in cols:
+        return_schema = return_schema.add(StructField(feature, FloatType()))
+    print("Built return schema")
+
+    # must be spark df so that data is compatible with calculate_shap
+    spark_test_df = spark.createDataFrame(test_df)
+    shap_values = spark_test_df.mapInPandas(calculate_shap, schema=return_schema)
+    print("Got SHAP values")
+    end = time.time() - start
+    print("SHAP computation time: ", end)
+    return shap_values, explainer
+
+
+def plot_shap_force(shap_values, explainer, df, indexes):
+    for index in indexes:
+        shap.force_plot(explainer.expected_value[0], shap_values[index], df.iloc[index], show=True)
--- Scripts/split_data.py
+++ Scripts/split_data.py
@@ -1,9 +1,10 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import DataFrame
 
-spark = SparkSession.builder.appName("Pablo split test 1")\
-    .config("spark.driver.memory", "15g")\
+spark = SparkSession.builder.appName("Pablo split test 1") \
+    .config("spark.driver.memory", "15g") \
     .getOrCreate()
+
 
 # splits a given dataframe based on an optional keyword
 def split_dataset(df: DataFrame, args: list[str]):
@@ -17,6 +18,7 @@ def split_dataset(df: DataFrame, args: list[str]):
         training_data, test_data = match_keyword(df, specified_split)
     return training_data, test_data
 
+
 # do train/test split based on dataframe and given keyword
 def match_keyword(df: DataFrame, keyword: str):
     match keyword:
@@ -27,7 +29,6 @@ def match_keyword(df: DataFrame, keyword: str):
             print("Commencing random split 75:25")
             training_data, test_data = df.randomSplit([0.75, 0.25])
     return training_data, test_data
-
 
 
 # isolates particular attacks from the dataframe
@@ -49,6 +50,10 @@ def isolate_attacks(df: DataFrame, search_strings: list):
         remaining_df = df.exceptAll(isolated_df)
         return remaining_df, isolated_df
 
+
+# returns two dataframes:
+# isolated_df contains the data where the double GT label == the specified double attack_label
+# remaining_df contains all other data
 def isolate_gt_label(df: DataFrame, attack_label):
     isolated_df = df.filter(df["GT"] == attack_label)  # get df containing only specified attack
 
@@ -59,3 +64,16 @@ def isolate_gt_label(df: DataFrame, attack_label):
         # remaining_df (to be used as training df) contains all rows in test_df that are not in the isolated test_df
         remaining_df = df.exceptAll(isolated_df)
         return remaining_df, isolated_df
+
+
+# get index of random row of a pyspark dataframe where two columns are equivalent to a passed value
+def get_row_with_matching_cols_index(df: DataFrame, val, col_1: str, col_2: str):
+    index = None
+    df_collect = df.collect()
+    for row in df_collect:
+        if (row.__getitem__(col_1) == val) and (row.__getitem__(col_2) == val):
+            index = df_collect.index(row)
+            break
+    if index is None:
+        print("index not found for these values and columns")
+    return index
